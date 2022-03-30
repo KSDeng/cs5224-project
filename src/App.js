903145365d6e9ff7651901c5d1fb8431a627c87c
KSDeng <kaisheng.deng@outlook.com>
2022-03-30 17:00:58 +0800

    add cardeal resources

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import $ from 'jquery';
 import Button from 'react-bootstrap/Button';
 import SellCarPanel from './Components/SellCarPanel';
+import GetRequestComponent from './Components/GetRequestComponent';
 
 function App() {
   return (
   	//<Button variant="primary">Primary</Button>
-	<SellCarPanel />
+	// <SellCarPanel />
+    <GetRequestComponent />
   );
 }
 
